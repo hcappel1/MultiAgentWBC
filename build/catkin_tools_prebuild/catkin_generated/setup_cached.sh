86bf5acca2bbfbbb24006c8f983957b617a03b73
hcappel1 <hencappel1@gmail.com>
2020-02-19 14:28:52 -0600

    first commit

---
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/hcappel1/MultiAgentWBC/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export LUA_PATH=";;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua"
+export PWD="/home/hcappel1/MultiAgentWBC/build/catkin_tools_prebuild"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hcappel1/MultiAgentWBC/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/hcappel1/MultiAgentWBC/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
