cdf4ecb10300d340c76132008340e48e4e0005b5
hcappel1 <hencappel1@gmail.com>
2020-02-21 17:21:07 -0600

    finished urdf and added publisher node

---
+++ build/robot/CMakeFiles/control_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/control_node.dir/src/control_node.cpp.o"
+  "/home/hcappel1/MultiAgentWBC/devel/.private/robot/lib/robot/control_node.pdb"
+  "/home/hcappel1/MultiAgentWBC/devel/.private/robot/lib/robot/control_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/control_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/robot/catkin_generated/setup_cached.sh
+++ build/robot/catkin_generated/setup_cached.sh
@@ -8,7 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/hcappel1/MultiAgentWBC/devel/.private/robot:$CMAKE_PREFIX_PATH"
-export LUA_PATH=";;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua"
+export LD_LIBRARY_PATH="/home/hcappel1/MultiAgentWBC/devel/.private/robot/lib:$LD_LIBRARY_PATH"
+export LUA_PATH=";;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua"
+export PKG_CONFIG_PATH="/home/hcappel1/MultiAgentWBC/devel/.private/robot/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/hcappel1/MultiAgentWBC/build/robot"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/hcappel1/MultiAgentWBC/devel/.private/robot/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/hcappel1/MultiAgentWBC/src/robot:$ROS_PACKAGE_PATH"
