831b04fe8fef6bf8bb23784fc575b86621e65e34
hcappel1 <hencappel1@gmail.com>
2020-02-20 18:23:31 -0600

    created urdf for robot

---
+++ build/robot/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/hcappel1/MultiAgentWBC/devel/.private/robot:$CMAKE_PREFIX_PATH"
+export LUA_PATH=";;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua;/opt/ros/kinetic/share/lua/5.1/?.lua"
+export PWD="/home/hcappel1/MultiAgentWBC/build/robot"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hcappel1/MultiAgentWBC/devel/.private/robot/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/hcappel1/MultiAgentWBC/src/robot:$ROS_PACKAGE_PATH"
---
+++ devel/share/robot/cmake/robotConfig.cmake
@@ -0,0 +1,1 @@
+/home/hcappel1/MultiAgentWBC/devel/.private/robot/share/robot/cmake/robotConfig.cmake
